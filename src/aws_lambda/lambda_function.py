72f8842a6fc17c8c08d23f959d574096ae8a74f1
Luís <luis.fmso.negrao@gmail.com>
2025-11-06 19:15:49 +0100

    modified deploy-lambda.yaml

--- src/aws_lambda/lambda_function.py
+++ src/aws_lambda/lambda_function.py
@@ -3,6 +3,7 @@ from src.agent.orchestrator import orchestrate
 
 def lambda_handler(event, context):
     """Handle requests to AWS Lambda function"""
+    
     try:
         body = json.loads(event.get("body", "{}"))
         user_query = body.get("query", "")

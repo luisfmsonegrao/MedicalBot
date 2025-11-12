659bcce17f663f8397df15f1afcdbb50452fa515
Luís <luis.fmso.negrao@gmail.com>
2025-11-12 14:07:54 +0100

    Added monitoring of mean session length

--- src/monitoring/config.py
+++ src/monitoring/config.py
@@ -1,4 +1,5 @@
 TABLE_NAME = "medicalbot-cache"
 NAMESPACE = "medicalbot/monitoring"
 TIME_DELTA = 3600
-MONITORING_VARIABLES = {'feedback': {'positive': 'positive', 'negative': 'negative'},'task_status': {'positive': True, 'negative': False}}
+POSITIVE_RATE_METRICS = {'feedback': {'positive': 'positive', 'negative': 'negative'},'task_status': {'positive': True, 'negative': False}}
+MEAN_COUNT_METRICS = ['session_id']

c8ab373295a6e76041bcd8766a644919b9e390ae
Luís <luis.fmso.negrao@gmail.com>
2025-12-03 21:04:16 +0000

    Cleanup

--- src/monitoring/config.py
+++ src/monitoring/config.py
@@ -1,5 +1,5 @@
 TABLE_NAME = "medicalbot-cache"
-NAMESPACE = "medicalbot/experimental_v2"
+NAMESPACE = "medicalbot/prod"
 TIME_DELTA = 3600
 POSITIVE_RATE_METRICS = {'feedback': {'positive': 'positive', 'negative': 'negative'},'task_status': {'positive': True, 'negative': False}}
 MEAN_COUNT_METRICS = ['session_id']

806cbf01f5188c20e9bb704573a630b02d24dcb2
Luís <luis.fmso.negrao@gmail.com>
2025-12-06 16:15:20 +0000

    Added unit tests and automated test workflow

---
+++ test/monitoring/test_metrics.py
@@ -0,0 +1,19 @@
+from src.monitoring.metrics import aggregate_pos_neg
+
+def test_pos_neg_agg():
+    items = [{'status':'negative','feedback':'negative'},
+             {'status':'negative','feedback':'positive'},
+             {'status':'negative','feedback':'positive'},
+             {'status':'negative','feedback':'positive'}]
+    metric = 'feedback'
+    values = {'positive':'positive','negative':'negative'}
+    group_fields = ()
+    agg = aggregate_pos_neg(items,metric,values,group_fields)
+    assert len(agg.keys()) == 1
+    assert ("TOTAL",) in agg
+    assert agg[("TOTAL",)]["positive"] == 3
+    assert agg[("TOTAL",)]["negative"] == 1
+
+
+
+    

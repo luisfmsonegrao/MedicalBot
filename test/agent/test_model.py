476a2f5052ab9f15bb78228e7dba46612004a50f
Luís <luis.fmso.negrao@gmail.com>
2025-12-09 14:35:29 +0000

    Fixed test; updated dependencies

--- test/agent/test_model.py
+++ test/agent/test_model.py
@@ -1,8 +1,9 @@
-from src.agent.copd_classifier import get_prediction
+from src.agent.copd_classifier import predict_copd
+from src.agent.tool_input_models import PredictionInput
 
 def test_get_prediction():
-    valid_features = {"age":22,"sex":"Female","smoker":"Yes","bmi":18}
-    prediction,_ = get_prediction(valid_features)
+    valid_features = PredictionInput(age=22,sex="Female",smoker="Yes",bmi=19)
+    prediction,_ = predict_copd(valid_features)
     classes = ['A','B','C','D']
     assert len(prediction) == 1
     assert prediction.__class__.__name__ == "ndarray"

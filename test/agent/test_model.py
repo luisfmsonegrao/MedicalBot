bdf551c251c5676e02b3ac83e876b28b1c75f0f5
Luís <luis.fmso.negrao@gmail.com>
2025-12-09 14:42:43 +0000

    Fixed test

--- test/agent/test_model.py
+++ test/agent/test_model.py
@@ -3,7 +3,7 @@ from src.agent.tool_input_models import PredictionInput
 
 def test_get_prediction():
     valid_features = PredictionInput(age=22,sex="Female",smoker="Yes",bmi=19)
-    prediction,_ = predict_copd(valid_features)
+    prediction = predict_copd(valid_features)
     classes = ['A','B','C','D']
     assert len(prediction) == 1
     assert prediction.__class__.__name__ == "ndarray"
